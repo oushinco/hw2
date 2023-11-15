6ba374fb7864b9db3d5489af4fe292b3d047bab5
Xinguang Wang <wang1xin@police.nsw.gov.au>
2023-11-15 14:14:20 +1100

    update

--- start_service.sh
+++ start_service.sh
@@ -3,7 +3,7 @@
 
 
 # Start the Gradio web server
-python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload --port 7860 &
+python -m llava.serve.gradio_web_server --controller http://hw1:10000 --model-list-mode reload --port 7860 &
 
 # Keep the script running to keep the container alive
 tail -f /dev/null
